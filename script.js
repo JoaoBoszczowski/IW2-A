40b64d6d2a293f8c74c7ce44101744393b6afe01
Aluno.Etec <aula.etec.com>
2026-02-11 15:23:59 -0300

    .

--- script.js
+++ script.js
@@ -20,3 +20,8 @@ let testeundefined;
 console.log(testenull)
 console.log(testeundefined)
 console.log(testeundefined *2)
+
+console.log("6" + 4);
+console.log("6" - 4);
+console.log("6" * 4);
+console.log("6" / 4);
