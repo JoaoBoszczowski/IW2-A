c9e6ba6cfb53c335a012573546c5e73ac1e3964e
Aluno.Etec <aula.etec.com>
2026-02-11 15:21:13 -0300

    aula 11 do 2

---
+++ script.js
@@ -0,0 +1,22 @@
+let nome = "joao";
+let nome2 = 'pedro'
+
+console.log(nome)
+console.log(nome2)
+console.log("meu nome é: " + nome)
+
+let num = 16;
+
+console.log(num)
+console.log("minha idade é:" + num)
+
+let maiorde18anos = true;
+
+console.log(maiorde18anos);
+
+let testenull = null
+let testeundefined;
+
+console.log(testenull)
+console.log(testeundefined)
+console.log(testeundefined *2)
