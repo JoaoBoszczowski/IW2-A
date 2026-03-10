cd2f537f799ca3dd513329fcf89119e362d811bd
JoaoBoszczowski <joaopedroboszczowskistabile@gmail.com>
2026-03-10 18:05:01 -0300

    Create 10\03.js

---
+++ 10\03.js
@@ -0,0 +1,18 @@
+let h1 = document.getElementById("ola-mundo");
+console.log(h1);
+
+
+let p = document.querySelector(".paragrafo");
+console.log (p)
+
+
+h1.textContent = "texte bla bla bla;"
+
+p.textContent = "bla bla bla"
+p.style.color = "green"
+
+let div = document.querySelector(".container");
+
+div.style.backgroundColor = "red"
+
+console.log (div);
